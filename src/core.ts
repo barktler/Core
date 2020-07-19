c306c93b69aa51fa59c1a02e2b871eb0c57b7388
WMXPY <wm@mengw.io>
2020-07-19 14:24:27 -0500

    add send request protected

--- src/core.ts
+++ src/core.ts
@@ -4,9 +4,19 @@
  * @description Core
  */
 
+import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
 export abstract class BarktlerCore {
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     protected constructor() {
+    }
+
+    protected async _sendRequest<T extends any = any>(request: AxiosRequestConfig): Promise<T> {
+
+        const response: AxiosResponse = await Axios(request);
+        const data: T = response.data;
+
+        return data;
     }
 }

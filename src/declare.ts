7c4a62a6b6df3e84b6e518056d3241775e517179
WMXPY <wm@mengw.io>
2020-07-22 11:48:37 -0500

    improve declare

--- src/declare.ts
+++ src/declare.ts
@@ -4,16 +4,32 @@
  * @description Declare
  */
 
-import { Method } from "axios";
+import { ResponseType } from "axios";
+
+export type Method =
+    | 'GET'
+    | 'DELETE'
+    | 'HEAD'
+    | 'OPTIONS'
+    | 'POST'
+    | 'PUT'
+    | 'PATCH'
+    | 'LINK'
+    | 'UNLINK';
 
 export interface IRequestConfig<Body extends any = any> {
 
     readonly url: string;
     readonly method: Method;
     readonly baseURL?: string;
-    readonly headers?: Record<string, string>;
 
+    readonly headers?: Record<string, string>;
+    readonly params?: Record<string, string>;
     readonly body?: Body;
+
+    readonly timeout?: number;
+
+    readonly responseType?: ResponseType;
 }
 
 export interface IResponseConfig<Data extends any = any> {

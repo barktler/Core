e876c795d83a5e2f0702d00219b50609fbde6381
WMXPY <wm@mengw.io>
2020-11-13 15:08:18 -0600

    implement pending request

--- src/declare.ts
+++ src/declare.ts
@@ -8,3 +8,6 @@ import { IRequestConfig, IResponseConfig } from "@barktler/driver";
 
 export type RequestVerifyOverrideFunction<RequestBody> = (request: IRequestConfig<RequestBody>) => void | Error;
 export type ResponseVerifyOverrideFunction<RequestBody, ResponseData> = (request: IRequestConfig<RequestBody>, response: IResponseConfig<ResponseData>) => void | Error;
+
+export type TriggerPostHookFunction<ResponseData> = (response: IResponseConfig<ResponseData>) => Promise<IResponseConfig<ResponseData> | null>;
+export type TriggerErrorHookFunction = (reason: any) => Promise<any>;
--- src/pending.ts
+++ src/pending.ts
@@ -5,24 +5,76 @@
  */
 
 import { IResponseConfig, PendingRequest } from "@barktler/driver";
-import { AsyncDataHook } from "@sudoo/processor";
+import { TriggerErrorHookFunction, TriggerPostHookFunction } from "./declare";
 
 export class HookedPendingRequest<ResponseData extends any = any> {
 
     // eslint-disable-next-line @typescript-eslint/no-shadow
-    public static create<ResponseData extends any = any>(pendingRequest: PendingRequest, postHook: AsyncDataHook<IResponseConfig<ResponseData>>, errorHook: AsyncDataHook<any>): HookedPendingRequest {
+    public static create<ResponseData extends any = any>(
+        pendingRequest: PendingRequest,
+        triggerPostHook: TriggerPostHookFunction<ResponseData>,
+        triggerErrorHook: TriggerErrorHookFunction,
+    ): HookedPendingRequest {
 
-        return new HookedPendingRequest(pendingRequest, postHook, errorHook);
+        return new HookedPendingRequest(pendingRequest, triggerPostHook, triggerErrorHook);
     }
 
     private readonly _pendingRequest: PendingRequest;
-    private readonly _postHook: AsyncDataHook<IResponseConfig<ResponseData>>;
-    private readonly _errorHook: AsyncDataHook<any>;
+    private readonly _triggerPostHook: TriggerPostHookFunction<ResponseData>;
+    private readonly _triggerErrorHook: TriggerErrorHookFunction;
 
-    private constructor(pendingRequest: PendingRequest, postHook: AsyncDataHook<IResponseConfig<ResponseData>>, errorHook: AsyncDataHook<any>) {
+    private readonly _response: Promise<IResponseConfig<ResponseData>>;
+
+    private constructor(
+        pendingRequest: PendingRequest,
+        triggerPostHook: TriggerPostHookFunction<ResponseData>,
+        triggerErrorHook: TriggerErrorHookFunction,
+    ) {
 
         this._pendingRequest = pendingRequest;
-        this._postHook = postHook;
-        this._errorHook = errorHook;
+        this._triggerPostHook = triggerPostHook;
+        this._triggerErrorHook = triggerErrorHook;
+
+        this._response = new Promise<IResponseConfig<ResponseData>>((resolve: (data: IResponseConfig<ResponseData>) => void, reject: (reason: any) => void) => {
+
+            this._pendingRequest.response.then((data: IResponseConfig<ResponseData>) => {
+
+                return this._triggerPostHook(data);
+            }).then((hookedData: IResponseConfig<ResponseData>) => {
+
+                resolve(hookedData);
+            }).catch((reason: any) => {
+
+                this._triggerErrorHook(reason).then((errorReason: any) => {
+
+                    reject(errorReason);
+                }).catch((newReason: any) => {
+
+                    reject(newReason);
+                });
+            });
+        });
+    }
+
+    public get response(): Promise<IResponseConfig<ResponseData>> {
+        return this._response;
+    }
+
+    public get pending(): boolean {
+        return this._pendingRequest.pending;
+    }
+    public get completed(): boolean {
+        return this._pendingRequest.completed;
+    }
+    public get succeed(): boolean {
+        return this._pendingRequest.succeed;
+    }
+    public get failed(): boolean {
+        return this._pendingRequest.failed;
+    }
+
+    public abort(): void {
+
+        this._pendingRequest.abort();
     }
 }

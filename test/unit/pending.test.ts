c6fba60be8a4811ab811c2f3b8d250f234cee456
WMXPY <wm@mengw.io>
2020-11-13 15:28:17 -0600

    add error tests

--- test/unit/pending.test.ts
+++ test/unit/pending.test.ts
@@ -57,4 +57,95 @@ describe('Given {HookedPendingRequest} Class', (): void => {
 
         expect(response).to.be.deep.equal(responseResult);
     });
+
+    it('should be able to hook response', async (): Promise<void> => {
+
+        const newData: string = chance.string();
+        const responseResult: IResponseConfig = {
+            status: HTTP_RESPONSE_CODE.OK,
+            statusText: 'OK',
+            headers: {},
+            data: chance.string(),
+        };
+
+        const pendingRequest: PendingRequest = PendingRequest.create({
+            response: Promise.resolve(responseResult),
+            abort: () => void 0,
+        });
+
+        const hookedPendingRequest: HookedPendingRequest = HookedPendingRequest.create(
+            pendingRequest,
+            (value) => value,
+            async (value) => ({
+                ...value,
+                data: newData,
+            }),
+            async (value) => value,
+        );
+
+        const response: IResponseConfig | null = await hookedPendingRequest.response;
+
+        expect(response).to.be.deep.equal({
+            ...responseResult,
+            data: newData,
+        });
+    });
+
+    it('should be able to get error response', async (): Promise<void> => {
+
+        const errorMessage: string = chance.string();
+
+        const pendingRequest: PendingRequest = PendingRequest.create({
+            response: (async () => {
+                throw new Error(errorMessage);
+            })(),
+            abort: () => void 0,
+        });
+
+        const hookedPendingRequest: HookedPendingRequest = HookedPendingRequest.create(
+            pendingRequest,
+            (value) => value,
+            async (value) => value,
+            async (value) => value,
+        );
+
+        let responseError: any;
+
+        try {
+            await hookedPendingRequest.response;
+        } catch (err) {
+            responseError = err;
+        }
+
+        expect(responseError.toString()).to.be.equal(new Error(errorMessage).toString());
+    });
+
+    it('should be able to hook error response', async (): Promise<void> => {
+
+        const errorMessage: string = chance.string();
+
+        const pendingRequest: PendingRequest = PendingRequest.create({
+            response: (async () => {
+                throw new Error(chance.string());
+            })(),
+            abort: () => void 0,
+        });
+
+        const hookedPendingRequest: HookedPendingRequest = HookedPendingRequest.create(
+            pendingRequest,
+            (value) => value,
+            async (value) => value,
+            async () => new Error(errorMessage),
+        );
+
+        let responseError: any;
+
+        try {
+            await hookedPendingRequest.response;
+        } catch (err) {
+            responseError = err;
+        }
+
+        expect(responseError.toString()).to.be.equal(new Error(errorMessage).toString());
+    });
 });
